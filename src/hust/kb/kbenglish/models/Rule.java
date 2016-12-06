f370b2129b2e449255f10c0110d99a1176a82275
Longcnttbkhn <hoanglong180695@gmail.com>
2016-12-06 16:06:07 +0700

    create Rule, Rule Create level

---
+++ src/hust/kb/kbenglish/models/Rule.java
@@ -0,0 +1,8 @@
+package hust.kb.kbenglish.models;
+
+public class Rule {
+	private int idRule;
+	private Level currentLevel;
+	private Level goalLevel;
+	private Book bookNeed;
+}
---
+++ src/hust/kb/kbenglish/models/RuleCreateLevel.java
@@ -0,0 +1,10 @@
+package hust.kb.kbenglish.models;
+
+public class RuleCreateLevel {
+	private int idRule;
+	private int minScore;
+	private int maxScore;
+	private Level level;
+	private String explain;
+	private int typeRole;
+}
