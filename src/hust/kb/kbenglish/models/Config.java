55b9c201aba0ca7aa06fe98ef95fa8cbf0561671
DucVuMinh <vuminhduc0103195@gmail.com>
2016-12-08 14:11:22 +0700

    add image

--- src/hust/kb/kbenglish/models/Config.java
+++ src/hust/kb/kbenglish/models/Config.java
@@ -3,5 +3,5 @@ package hust.kb.kbenglish.models;
 public class Config {
 	public static String DATABASE = "jdbc:mariadb://localhost:3306/knowledgebasedsystem";
 	public static String USER_NAME = "root";
-	public static String PASSWORD = "123456";
+	public static String PASSWORD = "";
 }
